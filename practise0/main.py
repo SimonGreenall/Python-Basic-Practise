cac930a2c54a31f243c925f6440c140b07db4703
SimonGreenall <739719464@qq.com>
2023-07-26 16:52:11 +0800

    modified:   practise0/main.py

--- practise0/main.py
+++ practise0/main.py
@@ -1,9 +1,11 @@
 while True:
     try:
-        a = float(input("请输入第一个数字"))
-        b = float(input("请输入第二个数字"))
-        print(a / b)
-        break
+        a = float(input("请输入第一个数字(输入q退出系统)"))
+        b = float(input("请输入第二个数字(输入q退出系统)"))
+        if(a=='q'):
+            break
+        else:
+            print(a/b)
     except ZeroDivisionError:
         print("分母不可为0,重新输入！")
         continue
